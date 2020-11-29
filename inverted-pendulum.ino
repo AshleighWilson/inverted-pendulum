ca43a49e35084f532623a6104755805c2d7ca3e0
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2020-11-29 16:08:07 +0000

    Initial hardware configuration for Arduino Motor Shield Rev 3.

--- inverted-pendulum.ino
+++ inverted-pendulum.ino
@@ -1,5 +1,17 @@
+#include <Arduino.h>
+
+// Hardware setup for Arduino Motor Shield Rev 3
+#define DIR_PIN 12
+#define SPEED_PIN 3
+#define BRAKE_PIN 9
+#define CURFB_PIN 0
 
 void setup() {
+    Serial.begin(115200);
+    pinMode(DIR_PIN, OUTPUT);
+    pinMode(SPEED_PIN, OUTPUT);
+    pinMode(BRAKE_PIN, OUTPUT);
+    pinMode(CURFB_PIN, INPUT);
 }
 
 void loop() {
